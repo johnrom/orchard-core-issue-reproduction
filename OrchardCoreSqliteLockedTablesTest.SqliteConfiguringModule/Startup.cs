a7b551a8b4f83f47e4b2909456ee7c567a3f5480
John Rom <john@nimblelight.com>
2022-03-16 11:29:33 -0400

    Cleanup.

--- OrchardCoreSqliteLockedTablesTest.SqliteConfiguringModule/Startup.cs
+++ OrchardCoreSqliteLockedTablesTest.SqliteConfiguringModule/Startup.cs
@@ -1,34 +1,19 @@
 using System;
-using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using OrchardCore.Environment.Shell;
-using OrchardCore.Environment.Shell.Models;
 using OrchardCore.Modules;
-using YesSql;
 
 namespace OrchardCoreSqliteLockedTablesTest.SqliteConfiguringModule
 {
-    /// <summary>
-    /// Override the Sqlite Connection Factory to use an unpooled connection.
-    /// </summary>
     public class SqliteConfiguringStartup : StartupBase
     {
-        // We want to override the Sqlite Connection Factory before any other module can use it.
         public override int Order { get; } = -1000;
 
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddSecondaryYesSqlStore();
             services.AddRawSqliteConfiguration();
-        }
-
-        public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
         }
     }
 }
